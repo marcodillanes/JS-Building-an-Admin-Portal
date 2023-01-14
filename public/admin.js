1e16a15d664419dd98117ef280b35f1f86b5ecac
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-01-14 11:59:55 -0800

    dont know

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,38 @@
 
 // Your Code Here
+let root = document.querySelector('#root')
+
+const updateQuantity = async () => {
+    let res = await fetch('http://localhost:3001/listBooks')
+    let books = await res.json()
+
+    let ul = document.createElement('ul')
+
+    books.forEach(ube => {
+        let li = document.createElement('li')
+
+        let input = document.createElement('input')
+        input.type = 'text'
+        input.value = ube.quantity
+
+        let btn = document.createElement('button')
+        btn.textContent = 'Save'
+        btn.addEventListener('click', () => {
+            fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({'id': ube.id, 'quantity': `${input.value}`})
+            })
+        })
+
+        let p = document.createElement('p')
+        p.textContent = ube.title
+        
+        li.append(p, input, btn)
+        ul.append(li)
+    })
+
+    root.append(ul)
+}
+
+updateQuantity()
